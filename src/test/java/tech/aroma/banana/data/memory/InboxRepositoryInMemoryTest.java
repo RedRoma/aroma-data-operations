e58cb098c68b4eca60fed3d45d725627ff5e47ef
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-17 13:52:11 -0800

    Unit Test Additions: InboxRepositoryInMemoryTest

--- src/test/java/tech/aroma/banana/data/memory/InboxRepositoryInMemoryTest.java
+++ src/test/java/tech/aroma/banana/data/memory/InboxRepositoryInMemoryTest.java
@@ -36,7 +36,7 @@ import static org.junit.Assert.assertThat;
  *
  * @author SirWellington
  */
-@Repeat(10)
+@Repeat(50)
 @RunWith(AlchemyTestRunner.class)
 public class InboxRepositoryInMemoryTest 
 {
