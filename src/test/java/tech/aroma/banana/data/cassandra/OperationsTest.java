c8f651ae8d87874b17863dc1cba49c2d01989cd6
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-18 14:20:33 -0800

    Adding Operations to help measure Database Call latencies

---
+++ src/test/java/tech/aroma/banana/data/cassandra/OperationsTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2016 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.banana.data.cassandra;
+
+import java.util.concurrent.Callable;
+import org.apache.thrift.TException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.Assert.assertThat;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.NumberGenerators.longs;
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(5)
+@RunWith(AlchemyTestRunner.class)
+public class OperationsTest 
+{
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @Test
+    public void testMeasureOperation() throws Exception
+    {
+        long sleepTimeMillis = one(longs(5, 50));
+        
+        Callable operation = () -> 
+        {
+           Thread.sleep(sleepTimeMillis);
+           
+            System.out.println("Operation complete");
+            return null;
+        };
+        
+        long result = Operations.measureOperation(operation);
+        assertThat(result, greaterThanOrEqualTo(sleepTimeMillis));
+    }
+    
+    @Test
+    public void testMeasureOperationWhenOperationThrows() throws Exception
+    {
+        Callable operation = () ->
+        {
+           throw new TException();
+        };
+        
+        assertThrows(() -> Operations.measureOperation(operation))
+            .isInstanceOf(TException.class);
+        
+    }
+
+}
