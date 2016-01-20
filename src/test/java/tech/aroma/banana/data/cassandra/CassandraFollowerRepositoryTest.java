e50ab9ba6a597c776e8cbed1ed09dda98043e775
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-20 15:37:49 -0800

    Unit test additions: CassandraFollowerRepositoryTest

---
+++ src/test/java/tech/aroma/banana/data/cassandra/CassandraFollowerRepositoryTest.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2016 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.banana.data.cassandra;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import java.util.List;
+import java.util.function.Function;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import tech.aroma.banana.thrift.Application;
+import tech.aroma.banana.thrift.User;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
+import tech.sirwellington.alchemy.test.junit.runners.GenerateList;
+import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.GenerateString;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.mockito.Answers.RETURNS_MOCKS;
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+import static tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.UUID;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(10)
+@RunWith(AlchemyTestRunner.class)
+public class CassandraFollowerRepositoryTest
+{
+
+    @Mock(answer = RETURNS_MOCKS)
+    private Cluster cluster;
+
+    @Mock
+    private Session cassandra;
+    
+    private QueryBuilder queryBuilder;
+    
+    @Mock
+    private Function<Row, User> userMapper;
+    @Mock
+    private Function<Row, Application> applicationMapper;
+
+    @Mock
+    private ResultSet results;
+
+    @Mock
+    private Row row;
+
+    @GeneratePojo
+    private User user;
+
+    @GeneratePojo
+    private Application application;
+
+    @GenerateString(UUID)
+    private String userId;
+
+    @GenerateString(UUID)
+    private String appId;
+    
+    @GenerateList(Application.class)
+    private List<Application> apps;
+    
+    @GenerateList(User.class)
+    private List<User> followers;
+    
+    private CassandraFollowerRepository instance;
+    
+    @Before
+    public void setUp()
+    {
+        queryBuilder = new QueryBuilder(cluster);
+        
+        instance = new CassandraFollowerRepository(cassandra, queryBuilder, userMapper, applicationMapper);
+    }
+    
+    @DontRepeat
+    @Test
+    public void testConstructor() throws Exception
+    {
+        assertThrows(() -> new CassandraFollowerRepository(null, queryBuilder, userMapper, applicationMapper))
+            .isInstanceOf(IllegalArgumentException.class);
+        
+        assertThrows(() -> new CassandraFollowerRepository(cassandra, null, userMapper, applicationMapper))
+            .isInstanceOf(IllegalArgumentException.class);
+        
+        assertThrows(() -> new CassandraFollowerRepository(cassandra, queryBuilder, null, applicationMapper))
+            .isInstanceOf(IllegalArgumentException.class);
+        
+        assertThrows(() -> new CassandraFollowerRepository(cassandra, queryBuilder, userMapper, null))
+            .isInstanceOf(IllegalArgumentException.class);
+        
+    }
+
+    @Test
+    public void testSaveFollowing() throws Exception
+    {
+    }
+
+    @Test
+    public void testDeleteFollowing() throws Exception
+    {
+    }
+
+    @Test
+    public void testFollowingExists() throws Exception
+    {
+    }
+
+    @Test
+    public void testGetApplicationsFollowedBy() throws Exception
+    {
+    }
+
+    @Test
+    public void testGetApplicationFollowers() throws Exception
+    {
+    }
+
+}
