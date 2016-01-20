c30dbd5cb2b6b5eaafb48795555e840827e65988
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-20 14:19:05 -0800

    InboxRepositoryInMemoryTest: Increasing test repititions

--- src/test/java/tech/aroma/banana/data/memory/InboxRepositoryInMemoryTest.java
+++ src/test/java/tech/aroma/banana/data/memory/InboxRepositoryInMemoryTest.java
@@ -47,7 +47,7 @@ import static tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.
  *
  * @author SirWellington
  */
-@Repeat(50)
+@Repeat(100)
 @RunWith(AlchemyTestRunner.class)
 public class InboxRepositoryInMemoryTest 
 {
