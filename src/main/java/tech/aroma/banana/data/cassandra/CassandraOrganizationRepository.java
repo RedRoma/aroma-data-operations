2c5e3df4561f14da84d9b44b40a85fe8fc5f9f1e
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-20 22:02:35 -0800

    CassandraOrganizationRepository: Removing import statement

--- src/main/java/tech/aroma/banana/data/cassandra/CassandraOrganizationRepository.java
+++ src/main/java/tech/aroma/banana/data/cassandra/CassandraOrganizationRepository.java
@@ -65,7 +65,6 @@ import static tech.aroma.banana.data.cassandra.Tables.Organizations.USER_ROLES;
 import static tech.aroma.banana.data.cassandra.Tables.Organizations.WEBSITE;
 import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
 import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
-import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
 
 /**
  *
