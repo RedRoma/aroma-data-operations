214f8476ad064f6128dbbd2544d1b2b3d97eaca3
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-22 08:52:36 -0800

    Adding ModuleCassandraDevCluster

---
+++ src/main/java/tech/aroma/banana/data/cassandra/ModuleCassandraDevCluster.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.banana.data.cassandra;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import javax.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.sirwellington.alchemy.annotations.arguments.Required;
+
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+
+/**
+ * Provides bindings that connect to a Development Cassandra Cluster intended 
+ * for use with Development and Testing purposes.
+ *
+ * @author SirWellington
+ */
+public final class ModuleCassandraDevCluster extends AbstractModule
+{
+
+    private final static Logger LOG = LoggerFactory.getLogger(ModuleCassandraDevCluster.class);
+
+    @Override
+    protected void configure()
+    {
+    }
+
+    @Provides
+    @Singleton
+    Cluster provideCassandraCluster()
+    {
+        return Cluster.builder()
+            .addContactPoint("cassandra-01.aroma.tech")
+            .withCredentials("cassandra", "cassandra")
+            .build();
+    }
+
+    @Provides
+    @Singleton
+    Session provideCassandraSession(@Required Cluster cluster)
+    {
+        checkThat(cluster).is(notNull());
+
+        return cluster.connect("Banana");
+    }
+
+    @Provides
+    QueryBuilder provideCQLBuilder(@Required Cluster cluster)
+    {
+        checkThat(cluster).is(notNull());
+
+        return new QueryBuilder(cluster);
+    }
+
+}
