f072bb97001f9f459be7294348cb788a0f06741d
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-26 21:30:38 -0800

    MemoryTokenRepositoryTest: increasing test iterations

--- src/test/java/tech/aroma/banana/data/memory/MemoryTokenRepositoryTest.java
+++ src/test/java/tech/aroma/banana/data/memory/MemoryTokenRepositoryTest.java
@@ -47,7 +47,7 @@ import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThr
  *
  * @author SirWellington
  */
-@Repeat(10)
+@Repeat(100)
 @RunWith(AlchemyTestRunner.class)
 public class MemoryTokenRepositoryTest
 {
