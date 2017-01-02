28672d3bd378b7a96f7c32d606579b5d9b3a59ef
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-01 18:26:59 -0800

    Formats

--- src/main/java/tech/aroma/data/MessageRepository.java
+++ src/main/java/tech/aroma/data/MessageRepository.java
@@ -4,19 +4,15 @@ package tech.aroma.data;
 /*
  * Copyright 2016 RedRoma, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
-
 import java.util.List;
 import org.apache.thrift.TException;
 import sir.wellington.alchemy.collections.lists.Lists;
@@ -25,10 +21,9 @@ import tech.aroma.thrift.Message;
 import tech.aroma.thrift.TimeUnit;
 import tech.sirwellington.alchemy.annotations.arguments.Required;
 
-
 /**
  * Answers questions and performs actions relating to {@linkplain Message Messages}.
- * 
+ *
  * @author SirWellington
  */
 public interface MessageRepository
@@ -37,37 +32,37 @@ public interface MessageRepository
     default void saveMessage(@Required Message message) throws TException
     {
         LengthOfTime defaultTime = new LengthOfTime()
-        .setValue(1)
-        .setUnit(TimeUnit.WEEKS);
-        
+            .setValue(1)
+            .setUnit(TimeUnit.WEEKS);
+
         this.saveMessage(message, defaultTime);
     }
-    
+
     void saveMessage(@Required Message message, @Required LengthOfTime lifetime) throws TException;
 
     Message getMessage(@Required String applicationId, @Required String messageId) throws TException;
 
     void deleteMessage(@Required String applicationId, @Required String messageId) throws TException;
-    
+
     default void deleteAllMessages(@Required String applicationId) throws TException
     {
         List<TException> exceptions = Lists.create();
-        
+
         getByApplication(applicationId)
             .stream()
-            .forEach(message -> 
+            .forEach(message ->
             {
                 try
                 {
                     deleteMessage(applicationId, message.messageId);
                 }
-                catch(TException ex)
+                catch (TException ex)
                 {
                     exceptions.add(ex);
                 }
             });
-        
-        if(!Lists.isEmpty(exceptions))
+
+        if (!Lists.isEmpty(exceptions))
         {
             throw Lists.oneOf(exceptions);
         }
@@ -80,6 +75,6 @@ public interface MessageRepository
     List<Message> getByApplication(@Required String applicationId) throws TException;
 
     List<Message> getByTitle(@Required String applicationId, @Required String title) throws TException;
-    
+
     long getCountByApplication(@Required String applicationId) throws TException;
 }
