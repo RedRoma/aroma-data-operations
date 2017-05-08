de9fb4d8651debb13f2adb27ca750c39f548405a
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-07 23:31:17 -0700

    Creates Guice Module for Database Serializers

--- src/test/java/tech/aroma/data/sql/Resources.java
+++ src/test/java/tech/aroma/data/sql/Resources.java
@@ -4,22 +4,32 @@
 
 package tech.aroma.data.sql;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import com.google.inject.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import tech.aroma.data.sql.serializers.ModuleSerializers;
+import tech.aroma.thrift.Message;
 
 public class Resources
 {
 
     public static Logger LOG = LoggerFactory.getLogger(Resources.class);
 
-    private static final Injector injector = Guice.createInjector(new ModuleTesting());
+    private static final Injector injector = Guice.createInjector(new ModuleTesting(),
+                                                                  new ModuleSerializers());
 
 
     public static JdbcTemplate connectToDatabase()
     {
         return injector.getInstance(JdbcTemplate.class);
+    }
+
+    public static DatabaseSerializer<Message> getMessageSerializer()
+    {
+        TypeLiteral<DatabaseSerializer<Message>> literal = new TypeLiteral<DatabaseSerializer<Message>>()
+        {
+        };
+        return injector.getInstance(Key.get(literal));
     }
 }
