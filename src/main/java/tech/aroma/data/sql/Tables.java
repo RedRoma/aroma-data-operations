7f4521e35b64efb03edc541c09efdcb53b3a777c
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-10 06:55:01 -0700

    Adds Organization Table information

--- src/main/java/tech/aroma/data/sql/Tables.java
+++ src/main/java/tech/aroma/data/sql/Tables.java
@@ -42,4 +42,30 @@ public final class Tables
         public static final String TOTAL_MESSAGES = "total_messages";
         public static final String REQUEST_TIME = "request_time";
     }
+
+    @NonInstantiable
+    static class Organizations
+    {
+        static final String TABLE_NAME = "Organizations";
+        static final String TABLE_NAME_MEMBERS = "Organizations_Members";
+
+        static final String ORG_ID = "org_id";
+        static final String ORG_NAME = "org_name";
+        static final String OWNERS = "owners";
+        static final String ICON_LINK = "icon_link";
+        static final String INDUSTRY = "industry";
+        static final String EMAIL = "contact_email";
+        static final String GITHUB_PROFILE = "github_profile";
+        static final String STOCK_NAME = "stock_name";
+        static final String TIER = "tier";
+        static final String DESCRIPTION = "description";
+        static final String WEBSITE = "website";
+
+        static final String USER_ID = "user_id";
+        static final String USER_FIRST_NAME = "user_first_name";
+        static final String USER_MIDDLE_NAME = "user_middle_name";
+        static final String USER_LAST_NAME = "user_last_name";
+        static final String USER_ROLES = "user_roles";
+        static final String USER_EMAIL = "user_email";
+    }
 }
