354cb4ea18628b9ab027696746c44e58259fe64b
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-11 08:28:55 -0700

    Adds Unit tests

--- src/test/java/tech/aroma/data/sql/serializers/TokenSerializerTest.kt
+++ src/test/java/tech/aroma/data/sql/serializers/TokenSerializerTest.kt
@@ -16,15 +16,19 @@ package tech.aroma.data.sql.serializers
  * limitations under the License.
  */
 
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
-import tech.sirwellington.alchemy.test.junit.runners.*
-
-import org.junit.Assert.*
 import org.mockito.Mock
 import org.springframework.jdbc.core.JdbcOperations
+import tech.aroma.data.sql.asUUID
+import tech.aroma.data.sql.toTimestamp
 import tech.aroma.thrift.authentication.AuthenticationToken
+import tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows
+import tech.sirwellington.alchemy.test.junit.runners.*
 import tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.UUID
+import java.lang.IllegalArgumentException
 
 @RunWith(AlchemyTestRunner::class)
 class TokenSerializerTest
@@ -41,15 +45,62 @@ class TokenSerializerTest
     private lateinit var tokenId: String
 
     @GenerateString(UUID)
-    private lateinit var appId: String
+    private lateinit var ownerId: String
 
     @GenerateString(UUID)
     private lateinit var orgId: String
+
+    @GenerateString
+    private lateinit var statement: String
 
 
     @Before
     fun setUp()
     {
+        token.tokenId = tokenId
+        token.ownerId = ownerId
+        token.organizationId = orgId
+
+        instance = TokenSerializer()
     }
 
+
+    @Test
+    fun testSave()
+    {
+        instance.save(token, null, statement, database)
+
+        verify(database).update(statement,
+                                tokenId.asUUID(),
+                                ownerId.asUUID(),
+                                orgId.asUUID(),
+                                token.ownerName,
+                                token.timeOfCreation.toTimestamp(),
+                                token.timeOfExpiration.toTimestamp(),
+                                token.tokenType.toString(),
+                                token.status.toString())
+    }
+
+    @DontRepeat
+    @Test
+    fun testSaveWithBadArgs()
+    {
+        assertThrows { instance.save(null, null, statement, database) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+
+        assertThrows { instance.save(token, null, null, database) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+
+        assertThrows { instance.save(token, null, "", database) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+
+        assertThrows { instance.save(token, null, statement, null) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+
+        assertThrows {
+            val invalidToken = token.deepCopy().setTokenId(statement)
+            instance.save(invalidToken, null, statement, database)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+
+    }
 }
