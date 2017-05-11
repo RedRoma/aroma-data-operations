2400dc1b90c78f7e0d81e7beede5268c04dc11ca
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-11 13:11:53 -0700

    Adds application DDL

---
+++ src/main/resources/tech/aroma/sql/ddl/application_ddl.sql
@@ -0,0 +1,32 @@
+------------------------------------------------------------------------------
+-- APPLICATIONS PROVISIONED TO WORK WITH AROMA
+------------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Applications
+(
+    app_id                   UUID,
+    name                     TEXT,
+    app_description          TEXT,
+    organization_id          UUID,
+    organization_name        TEXT,
+    programming_language     TEXT,
+    time_provisioned         TIMESTAMP,
+    tier                     TEXT,
+    time_of_token_expiration TIMESTAMP,
+    app_icon_media_id        UUID,
+
+    PRIMARY KEY (app_id)
+);
+
+CREATE INDEX IF NOT EXISTS Applications_By_Organization
+    ON Applications (organization_id);
+
+-- USED FOR REMEMBERING AN Application's Owners
+CREATE TABLE IF NOT EXISTS ApplicationOwners
+(
+    app_id   UUID,
+    owner_id UUID,
+    since    TIMESTAMP DEFAULT now(),
+
+    PRIMARY KEY (app_id, owner_id)
+)
