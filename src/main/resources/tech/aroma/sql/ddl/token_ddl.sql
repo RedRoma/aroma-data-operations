fefc33f507fc57fb519c4ea3ed2691f8f3a3ddf5
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-11 07:27:44 -0700

    Adds Token DDL

---
+++ src/main/resources/tech/aroma/sql/ddl/token_ddl.sql
@@ -0,0 +1,27 @@
+---------------------------------------
+-- DDL FOR AROMA TOKENS
+--
+-- Tokens are used to authenticate Users
+-- and Applications
+---------------------------------------
+
+CREATE TABLE IF NOT EXISTS Tokens
+(
+    token_id           UUID,
+    owner_id           UUID,
+    organization_id    UUID,
+    owner_name         TEXT,
+    features           TEXT [],
+    time_of_expiration TIMESTAMP,
+    time_of_creation   TIMESTAMP DEFAULT now(),
+    token_type         TEXT,
+    token_staus        TEXT,
+
+    PRIMARY KEY (token_id)
+);
+
+CREATE INDEX IF NOT EXISTS Tokens_By_Type
+    ON Tokens (token_type);
+
+CREATE INDEX IF NOT EXISTS Tokens_By_Owner
+    ON Tokens (owner_id);
