0a42b4e3fbd3891d23da56ba29a6d2618289bdb6
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-11 16:12:13 -0700

    Adds SQL Scripts

--- src/main/resources/tech/aroma/sql/inserts/insert_application.sql
+++ src/main/resources/tech/aroma/sql/inserts/insert_application.sql
@@ -2,13 +2,14 @@
 -- CREATES OR UPDATES AN APPLICATION
 ------------------------------------------------------------------------------
 
-INSERT INTO applications (app_id, name, app_description, organization_id, programming_language, tier, time_of_token_expiration, app_icon_media_id)
+INSERT INTO applications (app_id, app_name, app_description, organization_id, programming_language, tier, time_of_token_expiration, app_icon_media_id)
 VALUES (?, ?, ?, ?, ?, ?, ?, ?)
 ON CONFLICT (app_id)
     DO UPDATE
-        SET name                     = EXCLUDED.name,
+        SET app_name                 = EXCLUDED.app_name,
             app_description          = EXCLUDED.app_description,
             organization_id          = EXCLUDED.organization_id,
+            owners                   = EXCLUDED.owners,
             programming_language     = EXCLUDED.programming_language,
             time_last_updated        = now(),
             tier                     = EXCLUDED.tier,
