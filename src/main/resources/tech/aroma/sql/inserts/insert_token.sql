554270171a5fcaee44e09659c07f8ff09508d4ee
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-11 12:37:53 -0700

    Fixes typo in SQL field

--- src/main/resources/tech/aroma/sql/inserts/insert_token.sql
+++ src/main/resources/tech/aroma/sql/inserts/insert_token.sql
@@ -2,5 +2,5 @@
 -- INSERTS A NEW TOKEN
 ------------------------------------------------------------------------------
 
-INSERT INTO tokens (token_id, owner_id, organization_id, owner_name, time_of_creation, time_of_expiration, token_type, token_staus)
+INSERT INTO tokens (token_id, owner_id, organization_id, owner_name, time_of_creation, time_of_expiration, token_type, token_status)
 VALUES (?, ?, ?, ?, ?, ?, ?, ?)
