10a243f52b8747730a94c184b0759554ef67759c
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-17 00:04:31 -0700

    Adds User DDL

---
+++ src/main/resources/tech/aroma/sql/ddl/user_ddl.sql
@@ -0,0 +1,34 @@
+---------------------------------------
+-- DDL FOR AROMA USERS
+--
+-- USERS ARE ACTUAL PEOPLE THAT INTERACT
+-- WITH THE AROMA SERVICE OR APP
+---------------------------------------
+
+CREATE TABLE IF NOT EXISTS Users
+(
+    user_id              UUID,
+    first_name           TEXT,
+    middle_name          TEXT,
+    last_name            TEXT,
+    full_name            TEXT,
+    email                TEXT[],
+    roles                TEXT[],
+    gender               TEXT,
+    birthdate            DATE,
+    profile_image_id     UUID,
+    github_profile       TEXT,
+    time_account_created TIMESTAMP DEFAULT now(),
+
+    PRIMARY KEY (user_id)
+);
+
+
+CREATE INDEX IF NOT EXISTS Users_By_Last_Name
+    ON Users (last_name);
+
+CREATE INDEX IF NOT EXISTS Users_By_First_Name
+    ON Users (first_name);
+
+CREATE INDEX IF NOT EXISTS Users_By_Email
+    ON Users (email);
