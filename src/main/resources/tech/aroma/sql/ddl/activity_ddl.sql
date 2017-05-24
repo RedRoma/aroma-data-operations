0d11ee89095164f803c16b2532ce45a70840c782
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-23 17:47:35 -0700

    Creates DDL for Activity table

---
+++ src/main/resources/tech/aroma/sql/ddl/activity_ddl.sql
@@ -0,0 +1,19 @@
+------------------------------------------------------------------------------
+-- TABLES TO STORE ACTIVITY THAT HAPPENS WITHIN AROMA
+-- THAT MAY BE OF INTEREST TO USERS
+------------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Activity
+(
+    recipient_user_id UUID,
+    event_id          UUID,
+    app_id            UUID,
+    actor_user_id     UUID,
+    time_of_event     TIMESTAMP,
+    serialized_event  TEXT,
+
+    PRIMARY KEY (recipient_user_id, event_id)
+);
+
+
+CREATE INDEX IF NOT EXISTS Activity_By_Application ON Activity(app_id);
