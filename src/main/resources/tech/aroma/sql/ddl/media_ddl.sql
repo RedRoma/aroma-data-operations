4102134f9e5f8696e711987333688ad59343f750
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-30 09:32:45 -0700

    Creates Media DDL

---
+++ src/main/resources/tech/aroma/sql/ddl/media_ddl.sql
@@ -0,0 +1,35 @@
+---------------------------------------
+-- DDL FOR AROMA MEDIA
+--
+-- Media includes images, videos, and other binary information
+-- used by Aroma.
+---------------------------------------
+
+CREATE TABLE IF NOT EXISTS Media
+(
+    media_id        UUID,
+    media_type      TEXT,
+    width           INT,
+    height          INT,
+    creation_time   TIMESTAMP DEFAULT now(),
+    expiration_time TIMESTAMP DEFAULT NULL,
+    data            BYTEA,
+
+    PRIMARY KEY (media_id)
+)
+;
+
+-- Used to store thumbnails for images,
+-- allowing for quick retrievals
+CREATE TABLE IF NOT EXISTS Media_Thumbnails
+(
+    media_id        UUID,
+    width           INT,
+    height          INT,
+    creation_time   TIMESTAMP DEFAULT now(),
+    expiration_time TIMESTAMP DEFAULT NULL,
+    data            BYTEA,
+
+    PRIMARY KEY (media_id, width, height)
+)
+;
