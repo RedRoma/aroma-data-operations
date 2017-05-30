08a140a5d0995f36e0d331266f0ddd268ec65e48
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-30 12:51:42 -0700

    Updates insert script

--- src/main/resources/tech/aroma/sql/inserts/insert_thumbnail.sql
+++ src/main/resources/tech/aroma/sql/inserts/insert_thumbnail.sql
@@ -2,5 +2,5 @@
 -- INSERTS A NEW IMAGE THUMBNAIL
 ------------------------------------------------------------------------------
 
-INSERT INTO Media_Thumbnails (media_id, width, height, data)
-VALUES (?, ?, ?, ?)
+INSERT INTO Media_Thumbnails (media_id, width, height, media_type, data)
+VALUES (?, ?, ?, ?, ?)
