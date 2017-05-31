f351ac725d044fe4e702f01128d458e8189f36dd
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-31 14:19:23 -0700

    Renames Followings table

--- src/main/resources/tech/aroma/sql/ddl/followings_ddl.sql
+++ src/main/resources/tech/aroma/sql/ddl/followings_ddl.sql
@@ -2,7 +2,7 @@
 -- STORES AN APP'S FOLLOWERS AND REMEMBERS WHAT APP A User Follows
 ------------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS Followers
+CREATE TABLE IF NOT EXISTS Followings
 (
     app_id         UUID,
     user_id        UUID,
@@ -13,5 +13,5 @@ CREATE TABLE IF NOT EXISTS Followers
 ;
 
 CREATE INDEX IF NOT EXISTS App_Followers
-    ON Followers (app_id)
+    ON Followings (app_id)
 ;
