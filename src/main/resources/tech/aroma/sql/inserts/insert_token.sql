92e1b61ac7f6b74a24b4a8d88b2c6c3796cbf085
Wellington Moreno <jwellington.moreno@gmail.com>
2017-08-02 11:07:47 -0700

    Fixes issue with updating token info.

--- src/main/resources/tech/aroma/sql/inserts/insert_token.sql
+++ src/main/resources/tech/aroma/sql/inserts/insert_token.sql
@@ -4,3 +4,10 @@
 
 INSERT INTO tokens (token_id, owner_id, organization_id, owner_name, time_of_creation, time_of_expiration, token_type, token_status)
 VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+ON CONFLICT
+    DO UPDATE
+        SET
+            owner_name         = EXCLUDED.owner_name,
+            time_of_expiration = EXCLUDED.time_of_expiration,
+            token_type         = EXCLUDED.token_type,
+            token_status       = EXCLUDED.token_status
